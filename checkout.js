52a85884fc058aa9d2c03895e56b7436aa8893b5
lukasMartinez <lukasmartinez869@gmail.com>
2025-05-12 10:24:39 +0200

    Checkout -html verbesser checkout.js sauberer

--- checkout.js
+++ checkout.js
@@ -11,7 +11,7 @@ if (cartData && cartData.items.length > 0) {
         checkoutItemsList.appendChild(li);
     });
 
-    checkoutTotal.textContent = `Gesamtsumme: ${cartData.total.toFixed(2)} €`;
+    checkoutTotal.textContent = `Total: ${cartData.total.toFixed(2)} €`;
 } else {
     document.getElementById("checkout-cart-items").textContent = "Dein Warenkorb ist leer.";
 }
